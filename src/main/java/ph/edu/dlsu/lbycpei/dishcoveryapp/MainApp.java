3f38d1fd5d09f6c0420b19ec7b79773fff03d253
Catlix <elisha_mielle_a_catiis@dlsu.edu.ph>
2025-08-02 13:54:02 +0800

    added css styling

--- src/main/java/ph/edu/dlsu/lbycpei/dishcoveryapp/MainApp.java
+++ src/main/java/ph/edu/dlsu/lbycpei/dishcoveryapp/MainApp.java
@@ -11,7 +11,6 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/ph/edu/dlsu/lbycpei/dishcoveryapp/MainMenu.fxml"));
         Scene scene = new Scene(loader.load(), 1000, 600);
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         primaryStage.setTitle("Dishcovery App");
         primaryStage.setScene(scene);
         primaryStage.show();
